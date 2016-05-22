8b3010db694ae63ca387a4dfcc820cd55acd63df
juan jose <chepe0794@hotmail.com>
2016-05-22 14:09:09 -0600

    tarea de CRUD

---
+++ WebApplication1/WebApplication1/Controllers/usuariosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class usuariosController : Controller
+    {
+        private UsuarioDBEntities db = new UsuarioDBEntities();
+
+        // GET: usuarios
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.usuario.ToListAsync());
+        }
+
+        // GET: usuarios/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = await db.usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: usuarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,nombre,apellido")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.usuario.Add(usuario);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        // GET: usuarios/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = await db.usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: usuarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,nombre,apellido")] usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        // GET: usuarios/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            usuario usuario = await db.usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            usuario usuario = await db.usuario.FindAsync(id);
+            db.usuario.Remove(usuario);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
